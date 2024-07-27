6d59ee60ddccd8f145439ca5db54f02067fe2290
bylampa <157304712+bylampa@users.noreply.github.com>
2024-07-27 14:55:53 +0300

    Update modification.js

--- plugins/modification.js
+++ plugins/modification.js
@@ -31,4 +31,68 @@
 	     Lampa.Storage.set('device_name', 'Lampa Uncensored');
     } 
 
+    Lampa.Storage.listener.follow('change', function (event) {
+      if (event.name == 'activity' && Lampa.Activity.active().component === 'bookmarks') {
+        setTimeout(function(){
+          Lampa.Controller.move('down');
+          Lampa.Controller.move('up');
+        },50)
+      }
+    });
+
+    var last_video_used = [];
+
+	// преобразуем jQuery в Native селектор
+		function jQueryToNative(jQuerySelector) {
+			if (typeof jQuerySelector === 'string') {
+				return document.querySelector(jQuerySelector);
+			} else if (jQuerySelector instanceof jQuery) {
+				return jQuerySelector.get(0);
+			} else {
+				return jQuerySelector;
+			}
+		}
+
+	// плеер играет
+	Lampa.PlayerVideo.listener.follow('play', function (e) {
+	        //получаем текст текущей серии из панельки плеера
+		setTimeout(function(){	
+			var currentPart = $('.player-info__name').text();
+			last_video_used[0] = currentPart;
+			//Lampa.Noty.show(last_video_used[0]);
+		}, 200)
+	});
+
+	// плеер закрыт
+	Lampa.Player.listener.follow('destroy', function () {
+		// попытки
+		var attempts = 0;
+		// задержка между переключением серий
+		setTimeout(function(){
+	        var element = document.querySelector('.player-panel__body');
+			// если панельки плеера нет, он закрыт
+	        if (!element) {
+	            //Lampa.Noty.show('Плеер закрыт');
+				// нужно проверить, какая активность, от этого зависит структура
+				if (Lampa.Activity.active().component == 'torrents') {
+					var prox = $('.scroll__body > div > div > div > div > div:contains("' + last_video_used[0] + '")').parent().parent().parent()
+				} else if (Lampa.Activity.active().component == 'online_mod') {
+					var prox = $('.scroll__body > div > div > div:contains("' + last_video_used[0] + '")').parent().parent()
+				} else if (Lampa.Activity.active().component == 'showy') {
+					var prox = $('.scroll__body > div > div > div > div:contains("' + last_video_used[0] + '")').parent().parent().parent()
+				} else if (Lampa.Activity.active().component == 'modss_online') {
+					var prox = $('.scroll__body > div > div > div > div:contains("' + last_video_used[0] + '")').parent().parent().parent()
+				} else if (Lampa.Activity.active().component == 'bwajs') {
+					var prox = $('.scroll__body > div > div > div > div:contains("' + last_video_used[0] + '")').parent().parent().parent()
+				} else if (Lampa.Activity.active().component == 'lampavod') {
+					var prox = $('.scroll__body > div > div > div > div:contains("' + last_video_used[0] + '")').parent().parent().parent()
+				}
+				var queryProxy = jQueryToNative(prox)
+				Lampa.Controller.focus(queryProxy)
+				Navigator.focus(queryProxy)	
+				//Lampa.Noty.show(queryProxy);
+	        }
+		}, 500)
+	});
+
 })();
