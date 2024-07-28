c29b3132b9528f0376d8148ff48539c71896cff0
bylampa <157304712+bylampa@users.noreply.github.com>
2024-07-28 09:37:26 +0300

    Update modification.js

--- plugins/modification.js
+++ plugins/modification.js
@@ -4,10 +4,16 @@
   /*  // Создаем элементы аудио
 var audioUpElement = document.createElement('audio');
 var audioDownElement = document.createElement('audio');
+var audioLeftElement = document.createElement('audio');
+var audioRightElement = document.createElement('audio');
+var audioOkElement = document.createElement('audio');
 
 // Устанавливаем источники звука
 audioUpElement.src = 'https://github.com/spreyo/clicket/raw/main/sounds/click.mp3';
 audioDownElement.src = 'https://github.com/spreyo/clicket/raw/main/sounds/click.mp3';
+audioLeftElement.src = 'https://github.com/spreyo/clicket/raw/main/sounds/click.mp3';
+audioRightElement.src = 'https://github.com/spreyo/clicket/raw/main/sounds/click.mp3';
+audioOkElement.src = 'https://github.com/spreyo/clicket/raw/main/sounds/click.mp3';
 
 // Обрабатываем события пульта ТВ
 document.onkeydown = function(event) {
@@ -18,6 +24,18 @@ document.onkeydown = function(event) {
   } else if (event.keyCode === 40) { // Кнопка "вниз"
     audioDownElement.currentTime = 0;
     audioDownElement.play();
+    
+  } else if (event.keyCode === 37) { // Кнопка "влево"
+    audioLeftElement.currentTime = 0;
+    audioLeftElement.play();
+    
+  } else if (event.keyCode === 39) { // Кнопка "вправо"
+    audioRightElement.currentTime = 0;
+    audioRightElement.play();
+
+  } else if (event.keyCode === 13) { // Кнопка "ОК"
+    audioOkElement.currentTime = 0;
+    audioOkElement.play();
     
   }
 };*/
